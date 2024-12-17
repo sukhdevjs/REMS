cb5b9169c9e485fc3dacda61e0c3b5c5f0b9b3c4
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-17 13:09:36 +0545

    new comit

---
+++ src/test/java/com/sukhdev/rems/RealEstateManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukhdev.rems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
