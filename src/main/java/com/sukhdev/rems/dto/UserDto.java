20f51da0fd16fe3f928a2e44817266ebf80f0e5f
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-21 09:36:18 +0545

    update

---
+++ src/main/java/com/sukhdev/rems/dto/UserDto.java
@@ -0,0 +1,54 @@
+package com.sukhdev.rems.dto;
+
+import com.sukhdev.rems.enums.UserRole;
+
+
+public class UserDto {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private UserRole userRole;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+//    public UserDto(Long id, String name, String email, UserRole userRole) {
+//        this.id = id;
+//        this.name = name;
+//        this.email = email;
+//        this.userRole = userRole;
+//    }
+}
