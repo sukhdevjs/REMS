2115f82df62b2c4ccf179fb906b223e2a722873d
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-21 09:35:17 +0545

    update

---
+++ src/main/java/com/sukhdev/rems/controller/AuthController.java
@@ -0,0 +1,28 @@
+package com.sukhdev.rems.controller;
+
+import com.sukhdev.rems.dto.SignupRequest;
+import com.sukhdev.rems.dto.UserDto;
+import com.sukhdev.rems.services.auth.AuthService;
+//import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+
+    @Autowired
+    private  AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signupCustomer(@RequestBody SignupRequest signupRequest){
+        UserDto createdCustomerDto = authService.createCustomer(signupRequest);
+        if (createdCustomerDto == null)return new ResponseEntity<>
+                ("customer not created", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(createdCustomerDto,HttpStatus.CREATED);
+    }
+
+}
