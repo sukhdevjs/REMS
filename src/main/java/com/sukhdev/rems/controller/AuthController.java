21427893dd60a5da347d2c4b163d476a11787e94
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-29 19:44:39 +0545

    updaed

--- src/main/java/com/sukhdev/rems/controller/AuthController.java
+++ src/main/java/com/sukhdev/rems/controller/AuthController.java
@@ -1,21 +1,44 @@
 package com.sukhdev.rems.controller;
 
+import com.sukhdev.rems.dto.AuthenticationRequest;
+import com.sukhdev.rems.dto.AuthenticationResponse;
 import com.sukhdev.rems.dto.SignupRequest;
 import com.sukhdev.rems.dto.UserDto;
+import com.sukhdev.rems.entity.User;
+import com.sukhdev.rems.repository.UserRepository;
 import com.sukhdev.rems.services.auth.AuthService;
 //import lombok.RequiredArgsConstructor;
+import com.sukhdev.rems.services.auth.UserService;
+import com.sukhdev.rems.services.utils.JwtUtils;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private final UserService userService;
 
-    @Autowired
-    private  AuthService authService;
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtUtils jwtUtils;
+
+    private final UserRepository userRepository;
+
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signupCustomer(@RequestBody SignupRequest signupRequest){
@@ -27,6 +50,29 @@ public class AuthController {
         return new ResponseEntity<>(createdCustomerDto,HttpStatus.CREATED);
     }
 
+    @PostMapping("/login")
+    public AuthenticationResponse createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws BadCredentialsException, DisabledException, UsernameNotFoundException {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.getEmail(),
+                    authenticationRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            throw new BadCredentialsException("Incowrect Username or Password");
+        }
+        final UserDetails userDetails = userService.userDetailsService().loadUserByUsername(authenticationRequest.getEmail());
+        Optional<User> optionalUser = userRepository.findFirstByEmail(userDetails.getUsername());
+        final String jwt = jwtUtils.generateToken(userDetails);
+        AuthenticationResponse authenticationResponse = new AuthenticationResponse();
+        if (optionalUser.isPresent()) {
+            authenticationResponse.setJwt(jwt);
+            authenticationResponse.setUserId(optionalUser.get().getId());
+            authenticationResponse.setUserRole(optionalUser.get().getUserRole());
+        }
+        return authenticationResponse;
+    }
+
+    }
 
 
-}
+
+
