ebcad28d3771703c2e51878321292e801266bd76
Sukhdev Thakur <devil51sukhdev@gmail.com>
2024-12-30 19:25:25 +0545

    updated

--- src/main/java/com/sukhdev/rems/repository/UserRepository.java
+++ src/main/java/com/sukhdev/rems/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sukhdev.rems.repository;
 
 import com.sukhdev.rems.entity.User;
+import com.sukhdev.rems.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository <User,Long>{
 
     Optional<User> findFirstByEmail(String email);
+
+    User findByUserRole(UserRole userRole);
 }
