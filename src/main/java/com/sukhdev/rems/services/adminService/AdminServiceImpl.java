f6b1a3351662b853c5842643e1bee524d5d973d1
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-09 14:27:13 +0545

    wip

--- src/main/java/com/sukhdev/rems/services/adminService/AdminServiceImpl.java
+++ src/main/java/com/sukhdev/rems/services/adminService/AdminServiceImpl.java
@@ -4,10 +4,16 @@ import com.sukhdev.rems.dto.PropertyDto;
 import com.sukhdev.rems.entity.Property;
 import com.sukhdev.rems.repository.PropertyRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class AdminServiceImpl implements AdminService{
 
     private final PropertyRepository propertyRepository;
@@ -17,16 +23,24 @@ public class AdminServiceImpl implements AdminService{
         try{
             Property property = new Property();
             property.setName(propertyDto.getName());
-            property.setDescription(propertyDto.getAddress());
+            property.setDescription(propertyDto.getDescription());
             property.setAddress(propertyDto.getAddress());
-            property.setType(propertyDto.getType());
+            property.setPropertyType(propertyDto.getPropertyType());
+            property.setLocationType(propertyDto.getLocationType());
             property.setPrice(propertyDto.getPrice());
-            property.setImage(propertyDto.getImage().getBytes());
+            property.setImage(propertyDto.getImage() != null ? propertyDto.getImage().getBytes(): null);
             property.setNumberOfRooms(propertyDto.getNumberOfRooms());
             propertyRepository.save(property);
             return true;
         } catch (Exception e) {
+            log.error(e);
             return false;
         }
+    }
+
+    @Override
+    public List<PropertyDto> getAllProperty() {
+        return propertyRepository.findAll().stream().
+                map(Property::getPropertyDto).collect(Collectors.toList());
     }
 }
