f35e29f8cf75bdd27e9b255674f8044e8d638e89
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-09 14:26:33 +0545

    wip

--- src/main/java/com/sukhdev/rems/services/adminService/AdminService.java
+++ src/main/java/com/sukhdev/rems/services/adminService/AdminService.java
@@ -2,7 +2,11 @@ package com.sukhdev.rems.services.adminService;
 
 import com.sukhdev.rems.dto.PropertyDto;
 
+import java.util.List;
+
 public interface AdminService {
 
     boolean postProperty(PropertyDto propertyDto);
+
+    List<PropertyDto> getAllProperty();
 }
