4c9118b2b6332799d12b0dfb3be3ef640eaec486
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-09 14:22:48 +0545

    WiP

--- src/main/java/com/sukhdev/rems/configuration/CorsConfig.java
+++ src/main/java/com/sukhdev/rems/configuration/CorsConfig.java
@@ -5,13 +5,41 @@ package com.sukhdev.rems.configuration;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/api/**") // Apply to specific endpoints
+//                        .allowedOrigins("http://localhost:4200") // Frontend origin
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
+
+
 
 
 @Component
