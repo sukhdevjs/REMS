ff5202ccc42e5f4dde6fabf68c4004cb7d5e144c
Sukhdev Thakur <devil51sukhdev@gmail.com>
2025-01-09 14:24:12 +0545

    wip

--- src/main/java/com/sukhdev/rems/controller/AdminController.java
+++ src/main/java/com/sukhdev/rems/controller/AdminController.java
@@ -3,15 +3,18 @@ package com.sukhdev.rems.controller;
 import com.sukhdev.rems.dto.PropertyDto;
 import com.sukhdev.rems.repository.PropertyRepository;
 import com.sukhdev.rems.services.adminService.AdminService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +23,19 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/property")
-    public ResponseEntity<?> PostProperty(@ModelAttribute PropertyDto propertyDto){
+
+    @PostMapping(value = "/property", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> postProperty(@ModelAttribute PropertyDto propertyDto){
         boolean success = adminService.postProperty(propertyDto);
         if (success)
            return ResponseEntity.status(HttpStatus.CREATED).build();
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 
+    }
+
+    @GetMapping("/list-properties")
+    public ResponseEntity<List<PropertyDto>> getAllProperty(){
+        List<PropertyDto> propertyDtoList  = adminService.getAllProperty();
+        return ResponseEntity.ok(propertyDtoList);
     }
 }
